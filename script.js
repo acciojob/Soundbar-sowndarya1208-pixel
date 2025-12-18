a190ae397d0c868755fa62e0a896f7ac67f80767
sowndarya1208-pixel <sowndarya1208@gmail.com>
2025-12-18 21:30:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button');
+    btn.classList.add('btn');
+    btn.innerText = sound;
+
+    btn.addEventListener('click', () => {
+        stopSongs();
+        document.getElementById(sound).play();
+    });
+
+    document.getElementById('buttons').appendChild(btn);
+});
+
+// Create and add the Stop button
+const stopBtn = document.createElement('button');
+stopBtn.classList.add('btn', 'stop');
+stopBtn.innerText = 'stop';
+
+stopBtn.addEventListener('click', () => {
+    stopSongs();
+});
+
+document.getElementById('buttons').appendChild(stopBtn);
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound);
+        song.pause();
+        song.currentTime = 0; // Resets audio to beginning
+    });
+}
